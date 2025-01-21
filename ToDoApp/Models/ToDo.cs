8f5f52c82ba66ff026bc63ba888a51def2f8fa32
Nabin593 <callmegnabin@gmail.com>
2025-01-21 22:08:36 +0545

    fist commit with models added

---
+++ ToDoApp/Models/ToDo.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoApp.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Please Enter the Description.")]
+        public string Description { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Please Enter a DueDate.")]
+        public DateTime? DueDate { get; set; }
+        [Required(ErrorMessage = "Please Select a Category.")]
+        public string CategoryId { get; set; } = string.Empty;
+    }
+}
